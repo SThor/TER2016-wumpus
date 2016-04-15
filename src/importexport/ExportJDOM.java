e566fb627e4dd6276ad6377ab478d22ccc6d060a
SThor <paulgivel@gmail.com>
2016-04-15 20:56:32 +0200

    fixes on Export creation of the import functionnality

--- src/importexport/ExportJDOM.java
+++ src/importexport/ExportJDOM.java
@@ -9,7 +9,6 @@ import model.ObjectObservation;
 import model.ObjectProperty;
 import model.SysObject;
 import model.World;
-import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.output.Format;
@@ -45,7 +44,6 @@ public class ExportJDOM {
     public void exportAll() throws IOException {
         root.addContent(exportObjects());
         root.addContent(exportActions());
-        root.addContent(exportObservations());
         save(file);
     }
 
@@ -69,16 +67,6 @@ public class ExportJDOM {
         return actions;
     }
 
-    private Element exportObservations() {
-        Element observations = new Element("observations");
-
-        for (ObjectObservation observation : world.getObservations()) {
-            observations.addContent(exportObservation(observation));
-        }
-
-        return observations;
-    }
-
     private Element exportObject(SysObject object) {
         Element xmlObject = new Element("object");
         xmlObject.setAttribute("name", object.getName());
@@ -93,19 +81,21 @@ public class ExportJDOM {
     private Element exportProperty(ObjectProperty property) {
         Element xmlProperty = new Element("property");
 
+        Element xmlValues = new Element("values");
         for (String value : property.getPossibleValues()) {
             Element xmlValue = new Element("value");
             xmlValue.setAttribute("name", value);
-            xmlProperty.addContent(xmlValue);
+            xmlValues.addContent(xmlValue);
         }
+        xmlProperty.addContent(xmlValues);
 
         return xmlProperty;
     }
 
     private Element exportAction(Action action) {
         Element xmlAction = new Element("action");
-        Attribute name = new Attribute("name", action.getName());
-
+        xmlAction.setAttribute("name", action.getName());
+        
         Element xmlPre = new Element("preconditions");
         for (Condition preCondition : action.getPreConditions()) {
             xmlPre.addContent(exportCondition(preCondition));
@@ -127,12 +117,6 @@ public class ExportJDOM {
         xmlCondition.setAttribute("property", condition.getPropertyName());
         xmlCondition.setAttribute("wanted_value", condition.getWantedValue());
         return xmlCondition;
-    }
-
-    private Element exportObservation(ObjectObservation observation) {
-        Element xmlObservation = new Element("observation");
-        //TODO observation
-        return xmlObservation;
     }
 
     private void save(Path file) throws IOException {

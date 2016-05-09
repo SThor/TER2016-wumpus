903c2eb4194ef6193ddb545ed4ee7173d30e4b2a
Wierin <guillaume.hartenstein@gmail.com>
2016-05-09 11:35:54 +0200

    Minor fix for backtrack solver

--- src/solver/BacktrackSolver.java
+++ src/solver/BacktrackSolver.java
@@ -27,48 +27,46 @@ import model.observations.Scenario;
  */
 public class BacktrackSolver extends Solver {
     private IlcSolver solver;
-    private final Map<SysObject, Map<String, IlcAnyVar>> worldMap;
-    private final List<IlcAnyVar> varList;
+    private final List<Map<SysObject, Map<String, IlcAnyVar>>> worldMaps;
+    private final List<List<IlcAnyVar>> varList;
 
     public BacktrackSolver(World world, Scenario scenario) throws IloException {
         super(world, scenario);
         
         solver = new IlcSolver();
-        worldMap = new HashMap<>();
+        worldMaps = new ArrayList<>();
         varList = new ArrayList<>();
         
-        // Create all variables (one for each Object.property)
-        for (int i = 0; i < world.getObjectCount(); i++) {
-            SysObject object = world.getObjectAt(i);
-            Map<String, IlcAnyVar> objectMap = new HashMap<>();
-            worldMap.put(object, objectMap);
-            for (int j = 0; j < object.getPropertyCount(); j++) {
-                ObjectProperty property = object.getPropertyAt(i);
-                List<String> listValues = property.getPossibleValues();
-                IloAnyDomain propDomain = solver.anyDomain(listValues.toArray(new String[listValues.size()])); // Create a domain for every property
-                IlcAnyVar var = solver.anyVar(propDomain, object.getName()+"."+property.getName());
-                objectMap.put(property.getName(), var);
-                varList.add(var);
+        // Create all variables (one for each Object.property at each instant)
+        for (int k = 0; k < scenario.size(); k++) {
+            Map<SysObject, Map<String, IlcAnyVar>> mapAtInstant = new HashMap<>();
+            worldMaps.add(mapAtInstant);
+            List<IlcAnyVar> listAtInstant = new ArrayList<>();
+            varList.add(listAtInstant);
+            for (int i = 0; i < world.getObjectCount(); i++) {
+                SysObject object = world.getObjectAt(i);
+                Map<String, IlcAnyVar> objectMap = new HashMap<>();
+                mapAtInstant.put(object, objectMap);
+                for (int j = 0; j < object.getPropertyCount(); j++) {
+                    ObjectProperty property = object.getPropertyAt(j);
+                    List<String> listValues = property.getPossibleValues();
+                    IloAnyDomain propDomain = solver.anyDomain(listValues.toArray(new String[listValues.size()])); // Create a domain for every property
+                    IlcAnyVar var = solver.anyVar(propDomain, k+"_"+object.getName()+"."+property.getName());
+                    objectMap.put(property.getName(), var);
+                    listAtInstant.add(var);
+                }
             }
         }
     }
 
     @Override
     public List<Trajectory> solve() throws IloException {
-        
-        solver.add(scenario.get(0).solverConstraint(solver, worldMap));
-        solver.newSearch(solver.generate(varList.toArray(new IlcAnyVar[varList.size()])));
-        while (solver.next()) {
-            System.out.println(varList);
+        // Add constraints for each observations
+        for (int i = 0; i < scenario.size(); i++) {
+            solver.add(scenario.get(i).solverConstraint(solver, worldMaps.get(i)));
         }
-        /*for (Observation observation : scenario) {
-            solver.add(observation.solverConstraint(solver, worldMap));
-            solver.newSearch(solver.generate(varList.toArray(new IlcAnyVar[varList.size()])));
-            //... TODO : get the results in some form + get all possible solutions
-            solver = new IlcSolver(); // TODO : necessary ? or does newSearch() remove previous constraints ?
-        }*/
         
-        throw new UnsupportedOperationException("Not yet implemented.");
+        return null;
     }
 
     @Override
